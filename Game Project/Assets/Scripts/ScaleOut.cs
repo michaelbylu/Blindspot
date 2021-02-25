928b0f792012874ebf4f4ca43016a79ab59c77a0
Michael Lu <boyuanlu@andrew.cmu.edu>
2021-02-25 14:32:44 -0500

    Add jigsaw puzzle demo
    Add a puzzle playground scene with a 4-pics jigsaw puzzle demo

---
+++ Game Project/Assets/Scripts/ScaleOut.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleOut : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float maxScale;
+
+    public float speed;
+
+    public float pow;
+
+    private bool scaleUp = true;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(scaleUp) {
+            Vector3 newScale = transform.localScale + Vector3.one * speed * Time.deltaTime;
+            if(newScale.x >= maxScale){
+                transform.localScale = Vector3.one * maxScale;
+                scaleUp = false;
+                return;
+            }
+            transform.localScale = newScale;
+        }
+        else{
+            Vector3 newScale = transform.localScale - Vector3.one * speed * Time.deltaTime * 
+                Mathf.Pow(maxScale / transform.localScale.x, pow);
+            if(newScale.x <= 0){
+                Destroy(gameObject);
+            }
+            transform.localScale = newScale;
+        }
+    }
+}
