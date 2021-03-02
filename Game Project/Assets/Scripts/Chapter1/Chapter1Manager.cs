713ebacf1a25f0143de45a487c9c97245f642f85
Michael Lu <boyuanlu@andrew.cmu.edu>
2021-03-01 22:26:57 -0500

    Chapter 1 operational demo
    Hardcoded lines till second math problem.
    Player can move in the scene  with mouse click.
    (TODO - build an extendable dialogue system)

---
+++ Game Project/Assets/Scripts/Chapter1/Chapter1Manager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+public class Chapter1Manager : MonoBehaviour
+{
+    [System.Serializable]
+    public struct part {
+        public GameObject[] lines;
+    }
+    public part[] parts;
+    private int partIndex = 0;
+    private int lineIndex = 0;
+    void Start()
+    {
+        
+    }
+    private void OnEnable() {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void JumpTo(int line) {
+        GameObject target = parts[partIndex].lines[line];
+        if(target == null) {
+            return;
+        }
+        parts[partIndex].lines[lineIndex].SetActive(false);
+        target.SetActive(true);
+        gameObject.GetComponent<BoxCollider2D>().enabled = (target.GetComponentInChildren<Button>() == null);
+    }
+
+    public void JumpToNext() {
+        parts[partIndex].lines[lineIndex].SetActive(false);
+        if(lineIndex == parts[partIndex].lines.Length - 1) {
+            partIndex++;
+            lineIndex = 0;
+        }
+        else {
+            lineIndex++;
+        }
+        GameObject nextLine = parts[partIndex].lines[lineIndex];
+        nextLine.SetActive(true);
+        gameObject.GetComponent<BoxCollider2D>().enabled = (nextLine.GetComponentInChildren<Button>() == null);
+    }
+    public void HideCurrentLine() {
+        GameObject nextLine = parts[partIndex].lines[lineIndex];
+        nextLine.SetActive(false);
+    }
+
+    private void OnMouseUp() {
+        JumpToNext();
+    }
+}
---
+++ Game Project/Assets/Scripts/Chapter1/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public float moveSpeed = 2.0f;
+    private Vector3 screenPoint;
+    private bool isMoving = false;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        if(Input.GetKeyDown(KeyCode.P)) {
+            GameObject.FindGameObjectWithTag("GameController").GetComponent<Chapter1Manager>().JumpTo(0);
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+    private void OnMouseDown() {
+        screenPoint = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+        screenPoint.z = 0f;
+        isMoving = true;
+    }
+
+    private void Move() {
+        if(!isMoving) {
+            return;
+        }
+        gameObject.transform.position += moveSpeed * Time.deltaTime * 
+            Vector3.Normalize(screenPoint - gameObject.transform.position);
+        if(Vector3.Distance(gameObject.transform.position, screenPoint) <= 0.1f) {
+            gameObject.transform.position = screenPoint;
+            isMoving = false;
+        }
+    }
+}
