9727305ff1287334ba2ceb183271586570dd4767
Michael Lu <boyuanlu@andrew.cmu.edu>
2021-03-09 20:35:17 -0500

    Add blocker text movement
    Enabled blocker texts in puzzle1-2 to float and block the path that player is dragging.
    Resized text sprite for better control.

---
+++ Game Project/Assets/Scripts/Chapter1/BlockController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockController : MonoBehaviour
+{
+    public float moveSpeed = 0.5f;
+    public float[] xRange;
+    public float[] yRange;
+    private Vector3 nextPath;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Float();
+    }
+
+    private void OnEnable() {
+        nextPath = new Vector3(Random.Range(xRange[0], xRange[1]), Random.Range(yRange[0], yRange[1]), 0f);
+    }
+
+    private void Float() {
+        transform.position += (nextPath - transform.position).normalized * moveSpeed * Time.deltaTime;
+        if(Vector3.Distance(transform.position, nextPath) <= 0.1f) {
+            nextPath = new Vector3(Random.Range(xRange[0], xRange[1]), Random.Range(yRange[0], yRange[1]), 0f);
+        }
+    }
+}
