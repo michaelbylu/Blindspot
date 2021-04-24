6290824469f6c73936b71eb0048a4e38b3797702
Michael Lu <boyuanlu@andrew.cmu.edu>
2021-04-24 18:25:52 -0400

    Update intro button ui
    Added outline for intro buttons based on player's progress.

--- Game Project/Assets/Scripts/Intro/SceneButtons.cs
+++ Game Project/Assets/Scripts/Intro/SceneButtons.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class SceneButtons : MonoBehaviour
 {
+    public Button chapter1;
     public Button chapter2;
     public Button chapter3;
     // Start is called before the first frame update
@@ -25,5 +26,21 @@ public class SceneButtons : MonoBehaviour
         }
         chapter2.interactable = PlayerPrefs.HasKey("1");
         chapter3.interactable = PlayerPrefs.HasKey("2");
+        if(PlayerPrefs.HasKey("1")) {
+            if(PlayerPrefs.HasKey("2")) {
+                chapter2.GetComponent<Outline>().effectDistance = new Vector2(0f, 0f);
+                chapter3.GetComponent<Outline>().effectDistance = new Vector2(4f, 4f);
+            }
+            else {
+                chapter3.GetComponent<Outline>().effectDistance = new Vector2(0f, 0f);
+                chapter2.GetComponent<Outline>().effectDistance = new Vector2(4f, 4f);
+            }
+            chapter1.GetComponent<Outline>().effectDistance = new Vector2(0f, 0f);
+        }
+        else{
+            chapter1.GetComponent<Outline>().effectDistance = new Vector2(4f, 4f);
+            chapter2.GetComponent<Outline>().effectDistance = new Vector2(0f, 0f);
+            chapter3.GetComponent<Outline>().effectDistance = new Vector2(0f, 0f);
+        }
     }
 }
